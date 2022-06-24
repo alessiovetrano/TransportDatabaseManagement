98f1d511a0dba3b9712019f271860cff929d2f88
Luigi_Galluccio <74254368+LuigiGalluccio@users.noreply.github.com>
2022-06-24 14:57:39 +0200

    Update progettoDCL.sql

--- progettoDCL.sql
+++ progettoDCL.sql
@@ -1,6 +1,29 @@
 --PRIVILEGI DIRETTORE
 GRANT ALL PRIVILEGES TO utenteDirettore;
 
+
 --PRIVILEGI MANAGER
-GRANT SELECT ON  TO ManagerAziendale;
+GRANT SELECT ON SchemaUffici TO ManagerAziendale;
+GRANT SELECT ON SchemaVeicoliAssociati TO ManagerAziendale;
+GRANT SELECT ON VeicoliDisponibili TO ManagerAziendale;
+GRANT SELECT ON ImpiegatiInUfficio_attuale TO ManagerAziendale;
+GRANT EXECUTE ON nuovoDirettore TO ManagerAziendale;
+GRANT EXECUTE ON ScheduleViaggio TO ManagerAziendale;
+GRANT EXECUTE ON contaOre TO ManagerAziendale;
+
+--PRIVILEGI AUTISTA
+GRANT SELECT ON SchemaVeicoliAssociati to AutistaAziendale;
+GRANT SELECT ON StoricoViaggi to AutistaAziendale;
+
+
+--PRIVILEGI IMPIEGATO
+GRANT SELECT ON SchemaUffici to ImpiegatoAziendale;
+GRANT SELECT ON ImpiegatiInUfficio_attuale TO ImpiegatoAziendale;
+
+
+
+
+
+
+
 
