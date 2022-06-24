6e5d96a89cdc6b644a6f57499c876140ceb585a1
Luigi_Galluccio <74254368+LuigiGalluccio@users.noreply.github.com>
2022-06-24 14:54:33 +0200

    Update Views.sql

--- Views.sql
+++ Views.sql
@@ -1,2 +1,22 @@
-CREATE OR REPLACE VIEW SchemaUffici 
-AS select cf_impiegato,ufficio_impiegato,num_impiegati from impiegato join ufficio on ufficio_impiegato = num_ufficio;
+CREATE OR REPLACE VIEW SchemaUffici AS
+select cf_impiegato,ufficio_impiegato,num_impiegati from impiegato join ufficio on ufficio_impiegato = num_ufficio;
+
+CREATE OR REPLACE VIEW SchemaVeicoliAssociati AS 
+select cf_autista,num_patente,targa as targa_veicolo_associato from autista join veicolo on targa=targa_autista
+
+CREATE OR REPLACE VIEW
+Veicoli_disponibili
+AS
+select targa,tipo_veicolo,casa_produttrice from veicolo 
+minus 
+select targa,tipo_veicolo,casa_produttrice from veicolo join autista on targa=targa_autista;
+
+CREATE OR REPLACE VIEW ImpiegatiInUfficio_attuale
+AS
+select num_ufficio as numero_ufficio,count(*) as numero_attuale_impiegati from impiegato join ufficio on num_ufficio = ufficio_impiegato
+group by num_ufficio;
+
+CREATE OR REPLACE VIEW StoricoViaggi
+AS
+select data_viaggio,cf_viaggio 
+from viaggio join spedizione on data_viaggio = data_spedizione and cf_viaggio = cf_spedizione;
