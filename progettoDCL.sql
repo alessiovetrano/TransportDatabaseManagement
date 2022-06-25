a1d1bba3b1cbd01543ab89daac8cefc4bee9287f
Luigi_Galluccio <74254368+LuigiGalluccio@users.noreply.github.com>
2022-06-25 14:42:22 +0200

    Update progettoDCL.sql

--- progettoDCL.sql
+++ progettoDCL.sql
@@ -7,14 +7,13 @@ GRANT SELECT ON SchemaUffici TO ManagerAziendale;
 GRANT SELECT ON SchemaVeicoliAssociati TO ManagerAziendale;
 GRANT SELECT ON VeicoliDisponibili TO ManagerAziendale;
 GRANT SELECT ON ImpiegatiInUfficio_attuale TO ManagerAziendale;
+GRANT SELECT  ON StoricoViaggi to ManagerAziendale;
 GRANT EXECUTE ON ScheduleViaggio TO ManagerAziendale;
 GRANT EXECUTE ON contaOre TO ManagerAziendale;
-GRANT EXECUTE ON StoricoViaggi to ManagerAziendale;
 
 --PRIVILEGI AUTISTA
 GRANT SELECT ON SchemaVeicoliAssociati to AutistaAziendale;
 GRANT SELECT ON StoricoViaggi to AutistaAziendale;
-
 
 --PRIVILEGI IMPIEGATO
 GRANT SELECT ON SchemaUffici to ImpiegatoAziendale;
